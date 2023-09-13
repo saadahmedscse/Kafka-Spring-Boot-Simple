268a1292242e9aa3a75ccef6e8167cd532dce244
saadahmedscse <saadahmedscse@gmail.com>
2023-09-14 02:55:10 +0600

    created app constants util

---
+++ Consumer-Service/src/main/java/com/saadahmedev/consumerservice/util/AppConstants.java
@@ -0,0 +1,7 @@
+package com.saadahmedev.consumerservice.util;
+
+public class AppConstants {
+
+    public static final String MESSAGE_TOPIC = "message-topic";
+    public static final String GROUP_ID = "group-1";
+}
